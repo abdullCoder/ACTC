8e1acb1f171edca8d9b9ed33439a02b82ba0c2d4
abdullCoder <mukhtarabdurrahman95@gmail.com>
2023-05-25 02:53:26 -0700

    this is a commit to add photos

--- auth.js
+++ auth.js
@@ -100,7 +100,7 @@ function login () {
     database_ref.child('users/' + user.uid).update(user_data)
 
     // DOne
-    alert('User Logged In!!')
+    alert('Yoour successfully logged in')
     window.location.href = "exams_page.html"
 
   })
